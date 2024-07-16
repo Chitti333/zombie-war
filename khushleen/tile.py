0b70c9277e1c51d0d9144345cd17cade0769c552
khushleen <thekhushleenkaur.gmail.com>
2024-07-17 02:37:54 +0530

    draft after completing graphics

--- khushleen/tile.py
+++ khushleen/tile.py
@@ -5,6 +5,6 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/rock.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64,64))
+        #self.image = pygame.transform.scale(self.image, (64,64))
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10)
